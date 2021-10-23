36a1a35b2818f3a0d3c1166946937c83492da715
ljw <ljw@ljw.com>
2021-10-23 22:46:03 +0800

    add login ui

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/css/global.css'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+
+createApp(App)
+    .use(router)
+    .use(ElementPlus)
+    .mount('#app')
